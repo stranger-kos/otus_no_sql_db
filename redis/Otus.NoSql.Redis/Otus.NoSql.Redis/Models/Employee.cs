caeb572ffbf37bf86efdb0678c4dabeb2460af88
kushakov <ushakov-ke@yandex.ru>
2023-01-07 22:10:59 +0300

    redis hometask

---
+++ redis/Otus.NoSql.Redis/Otus.NoSql.Redis/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace Otus.NoSql.Redis.Models
+{
+    internal class Employee
+    {
+        private static Random _random = new Random();
+
+        public long Id { get; set; }
+
+        public int Salary { get; set; }
+
+        public static Employee Get(long id)
+        {
+            return new Employee
+            {
+                Id = id,
+                Salary = _random.Next()
+            };
+        }
+    }
+}
---
+++ redis/Otus.NoSql.Redis/Otus.NoSql.Redis/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using Otus.NoSql.Redis.Models;
+using ServiceStack.Redis;
+
+var values = new List<Employee>(5_000_00);
+
+for (int i = 0; i < 5_000_00; i++)
+{
+    var employee = Employee.Get(i);
+
+    values.Add(employee);
+}
+
+var redis = new RedisClient("localhost:6379");
+
+using var pipeline = redis.CreatePipeline();
+
+var watch = new Stopwatch();
+Console.WriteLine("Begin load...");
+watch.Start();
+
+foreach (var item in values)
+{
+    //    string
+    pipeline.QueueCommand(e => e.Add(item.Id.ToString(), item.Salary.ToString()));
+
+    //    hash
+    //pipeline.QueueCommand(e => e.SetEntryInHash(item.Id.ToString(), "salary", item.Salary.ToString()));
+
+    //    zset
+    //pipeline.QueueCommand(e => e.AddItemToSortedSet(item.Id.ToString(), item.Salary.ToString()));
+
+    //list
+    //pipeline.QueueCommand(e => e.AddItemToList(item.Id.ToString(), item.Salary.ToString()));
+}
+
+pipeline.Flush();
+
+watch.Stop();
+
+Console.WriteLine("Load time: " + watch.Elapsed);
+
+watch = new Stopwatch();
+Console.WriteLine("Begin Get...");
+watch.Start();
+
+//string
+var value = redis.GetValue("11525");
+
+//hash
+//var value = redis.GetAllEntriesFromHash("11525");
+
+//zset
+//var value = redis.GetAllItemsFromSortedSet("11525");
+
+//list
+//var value = redis.GetAllItemsFromList("11525");
+
+watch.Stop();
+Console.WriteLine("Get time: " + watch.Elapsed);
